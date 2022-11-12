9d89120821ddf2deaf4601153054d154d5f0161a
guravity <shogo.ogura365@gmail.com>
2022-11-12 12:10:18 +0900

    Hello func covers multiple languages.

--- project/hello.go
+++ project/hello.go
@@ -2,10 +2,33 @@ package main
 
 import "fmt"
 
-func Hello(name string) string{
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "es"
+const french = "fr"
+
+func Hello(name string, lang string) string { // public func
+	if name == "" {
+		name = "World"
+	}
+	
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string)(prefix string){ // pricate func
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return // automatically return prefix
 }
 
 func main() {
-	fmt.Println(Hello("Go World"))
+	fmt.Println(Hello("Go World", ""))
 }
--- project/hello_test.go
+++ project/hello_test.go
@@ -2,11 +2,37 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	got := Hello("Go World")
-	want := "Hello, Go World"
+func TestHello(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %q want %q", got , want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() // ヘルパーであることを明示
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to go", func(t *testing.T) {
+		got := Hello("Go World", "")
+		want := "Hello, Go World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello when alone", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "es")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Charlotte", "fr")
+		want := "Bonjour, Charlotte"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
