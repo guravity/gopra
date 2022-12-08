20e3b32609ac06e73faac6f9ac1bfde3bf0c3c4c
guravity <shogo.ogura365@gmail.com>
2022-12-08 11:45:34 +0900

    mocking done

--- project/mocking/countdown.go
+++ project/mocking/countdown.go
@@ -8,22 +8,51 @@ import (
 )
 const finalWord = "Go!"
 const countDownStart = 3
+const write = "write"
+const sleep = "sleep"
 
-type Sleeper interface { // Sleepをモックするためにインターフェース定義
+type Sleeper interface { // Sleepをモックするためのインターフェース定義
 	Sleep()
 }
 
 type SpySleeper struct {
 	Calls int
 }
-func (s *SpySleeper) Sleep(){ // インターフェースの実装
+func (s *SpySleeper) Sleep(){ // インターフェースのモック実装
 	s.Calls++ // メソッドが呼び出された回数をカウント
 }
 
-type DefaultSleeper struct {}
-func (d *DefaultSleeper) Sleep(){
-	time.Sleep(1*time.Second)
+type CountdownOperationsSpy struct { // →Sleeper, io.Writer のモック実装
+	Calls []string
 }
+func (s *CountdownOperationsSpy) Sleep(){
+	s.Calls = append(s.Calls, sleep) // 1つ目には追加元の配列
+}
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct { // timeのモック
+	durationSlept time.Duration
+}
+func (s *SpyTime) Sleep(duration time.Duration){ // time.sleepのモックメソッド
+	s.durationSlept = duration
+}
+
+// type DefaultSleeper struct {}
+// func (d *DefaultSleeper) Sleep(){
+// 	time.Sleep(1*time.Second)
+// }
+
+type ConfigurableSleeper struct { // カスタマブルなスリーパー
+	duration time.Duration // スリープ時間
+	sleep func(time.Duration)
+}
+func (c *ConfigurableSleeper) Sleep(){
+	c.sleep(c.duration)
+}
+
 
 func Countdown(out io.Writer, sleeper Sleeper){
 	// fmt.Fprint(out, "3") // バッファに書き込み
@@ -36,6 +65,6 @@ func Countdown(out io.Writer, sleeper Sleeper){
 }
 
 func main(){
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- project/mocking/countdown_test.go
+++ project/mocking/countdown_test.go
@@ -1,25 +1,58 @@
 package main
 
 import (
-	"testing"
 	"bytes"
+	"reflect"
+	"testing"
+	"time"
 )
 
-func TestCountdown(t *testing.T){
-	buffer := &bytes.Buffer{} // テストできるように出力先をBufferに送信
-	spySleeper := &SpySleeper{}
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to go", func(t *testing.T) { // 表示のテスト
+		buffer := &bytes.Buffer{} // テストできるように出力先をBufferに送信
+		spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
-	
-	got := buffer.String()
-	want := "3\n2\n1\nGo!"
+		Countdown(buffer, spySleeper)
 
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
+
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+		if spySleeper.Calls != 4 {
+			t.Errorf("not enough calls to sleeper, want 4 but got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) { // スリープと表示の順序テスト
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurabelSleeper(t *testing.T){
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime{
+		t.Errorf("should have slept for %v but slept for %v",sleepTime, spyTime.durationSlept)
 	}
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 but got %d", spySleeper.Calls)
-	}
-
-
 }
